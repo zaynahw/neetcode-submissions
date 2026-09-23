9e9b2d0aa6325a664172b8d11cc03fa3d72295a1
Zaynah <zaynahwahab@gmail.com>
2026-09-22 23:01:55 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++){
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+            if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+
+    }
+}
